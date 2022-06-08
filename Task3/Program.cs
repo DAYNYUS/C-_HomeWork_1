cfbc21fc390909805553a14570ae339df7816add
DAYNYUS <excaliburbmw@mail.ru>
2022-06-08 12:21:40 +0200

    add task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 3. Напишите программу, которая на вход принимает число и выдает, является ли число четным 
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+if (number%2 == 0)
+{
+ Console.WriteLine("Число четное?");
+ Console.WriteLine("Да");
+}
+else 
+{
+ Console.WriteLine("Число четное?");
+ Console.WriteLine("Нет");
+}
