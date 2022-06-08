9c6cfd89c59fabfc5f603d905c1b778e6a95e88e
DAYNYUS <excaliburbmw@mail.ru>
2022-06-08 12:24:30 +0200

    add task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N (1 - 5; 1 - 8)
+Console.Write("Введите число N: ");
+string input1 = Console.ReadLine();
+int numberN = Convert.ToInt32(input1);
+
+int index = 1;
+while (index <= numberN)
+{
+
+    if(index%2 == 0)
+        {
+            Console.WriteLine(index);
+        }
+        index = index + 1;
+}
